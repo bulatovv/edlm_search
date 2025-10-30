6b53b8206b1de8009aab6af6ea093a6ca79e74f5
Albert Fazlyev <albert.fz@yandex.ru>
2025-10-30 22:16:44 +0500

    add idea field to candidate

--- src/edlm_search/candidate.py
+++ src/edlm_search/candidate.py
@@ -3,12 +3,13 @@ from edlm_search.problem import Problem
 
 
 class Candidate:
-    def __init__(self, files: dict[str, str]):
+    def __init__(self, files: dict[str, str], idea: str):
         self.files: dict[str, str] = files
+        self.idea = idea
 
     @classmethod
     async def new_from_problem(cls, problem: Problem, llm_pipeline: LLMPipeline):
-        files = await llm_pipeline.generate_files_from_template(
+        idea, files = await llm_pipeline.generate_files_from_template(
             'new_candidate', problem=problem
         )
-        return Candidate(files)
+        return Candidate(files, idea)
